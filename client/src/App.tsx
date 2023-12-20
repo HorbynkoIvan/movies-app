b8f83f2dfb5b8960162bd6ada4c9e601abe89d02
Ivan Horbynko <horbynko.i@gmail.com>
2023-12-20 14:30:15 +0200

    add basic navmenu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,78 @@
 import { BrowserRouter } from "react-router-dom";
-import { Button } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useState } from "react";
 
 function App() {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  const list = () => (
+    <Box sx={{ width: 250 }} role="presentation">
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
-      <Button variant="outlined">Outlined</Button>
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              // sx={{ mr: 2 }}
+              sx={{ display: { xl: "none", lg: "none" } }}
+              onClick={() => setDrawerOpen(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer anchor={"left"} open={isDrawerOpen} onClose={() => setDrawerOpen(false)}>
+          {list()}
+        </Drawer>
+      </Box>
     </BrowserRouter>
   );
 }
